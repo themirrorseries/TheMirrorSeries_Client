f25ab2f35e1fb790a4a1938a29ef464a0b3e3150
兔子 <liuxiang2@ztgame.com>
2019-08-25 19:31:38 +0800

    固定雷电朝向

--- Assets/Thunder.cs
+++ Assets/Thunder.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Thunder : MonoBehaviour
 {
+    private GameObject parent;
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+    }
+    void Update()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            transform.localEulerAngles = new Vector3(0, 360 - parent.transform.localEulerAngles.y, 0);
+        }
+    }
     public void HideThunder()
     {
         gameObject.SetActive(false);
